c471dbbbbd1c51e66e057c23a0058668e44df733
nnhuong <nnhuong1763@gmail.com>
2024-07-30 18:08:56 +0700

    Login

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,20 +1,62 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import './UserManage.scss';
+import {getAllUsers} from '../../services/userService';
 class UserManage extends Component {
 
-    state = {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            arrUsers: []
+        }
     }
 
-    componentDidMount() {
-
+    async componentDidMount() {
+        let response = await getAllUsers('ALL');
+        if(response && response.errCode === 0){
+            this.setState({
+                arrUsers: response.users   
+            })
     }
-
-
+                
+}
     render() {
+      console.log('check render', this.state)
+      let arrUsers = this.state.arrUsers
         return (
-            <div className="text-center">Manage users</div>
+            <div className="user-container">
+                <div className="title text-center">Manage users </div>
+                <div className="users-table mt-4 mx-1">
+<table id="customers">
+  <tr>
+    <th>Email</th>
+    <th>Firstname</th>
+    <th>Lastname</th>
+    <th>Address</th>
+    <th>Actions</th>
+  </tr>
+  
+    { arrUsers && arrUsers.map((item, index) => {
+      console.log('check map', item, index)
+      return (
+        <tr>
+          <td>{item.email}</td>
+          <td>{item.firstName}</td>
+          <td>{item.lastName}</td>
+          <td>{item.address}</td>
+          <td>
+            <button className="btn-edit"><i class="fas fa-pen-square"></i></button>
+            <button className="btn-delete"><i class="fas fa-trash"></i></button>
+          </td>
+        </tr>
+      )
+    })
+}
+  
+</table>
+                </div>
+            </div>
         );
     }
 
