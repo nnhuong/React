c414267fdb2199b015c74eef58ba31aa8f2b78be
nnhuong <nnhuong1763@gmail.com>
2024-08-06 23:22:08 +0700

    React-Slider-Carousel

--- src/containers/HomePage/Section/Specialty.js
+++ src/containers/HomePage/Section/Specialty.js
@@ -5,7 +5,31 @@ import { FormattedMessage } from 'react-intl';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+
+function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", background: "red" }}
+        onClick={onClick}
+      />
+    );
+  }
+  
+  function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", background: "green" }}
+        onClick={onClick}
+      />
+    );
+  } 
 class Specialty extends Component {
+    
+
 
 
     render() {
@@ -13,29 +37,31 @@ class Specialty extends Component {
             dots: false,
             infinite: true,
             speed: 500,
-            slidesToShow: 1,
-            slidesToSroll: 1
+            slidesToShow: 4,
+            slidesToSroll: 1,
+            // nextArrow: <SampleNextArrow/>,
+            // prevArrow: <SamplePrevArrow/>
         }
         return (
             <div className="section-specialty">
                 <div className="specialty-content">
                 <Slider {...settings}>
-                    <div>
+                    <div className="img-customize">
                         <h3>1</h3>
                     </div>
-                    <div>
+                    <div className="img-customize">
                         <h3>2</h3>
                     </div>
-                    <div>
+                    <div  className="img-customize">
                         <h3>3</h3>
                     </div>
-                    <div>
+                    <div className="img-customize">
                         <h3>4</h3>
                     </div>
-                    <div>
+                    <div  className="img-customize">
                         <h3>5</h3>
                     </div>
-                    <div>
+                    <div  className="img-customize">
                         <h3>6</h3>
                     </div>
                 </Slider>
