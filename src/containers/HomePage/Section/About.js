79aa380aa404545d3c801a1c19729da3d34c0b91
nnhuong <nnhuong1763@gmail.com>
2024-08-13 20:49:13 +0700

    design UX/UI

--- src/containers/HomePage/Section/About.js
+++ src/containers/HomePage/Section/About.js
@@ -7,7 +7,25 @@ class About extends Component {
     render() {
         return (
             <div className="section-share section-about">
-                    About us
+                <div className="section-about-header">
+                    Truyền thông nói gì về chúng tôi
+                </div>
+                <div className="section-about-content">
+                    <div className="content-left">
+                     <iframe 
+                     width="100%" 
+                     height="400px" 
+                     src="https://www.youtube.com/embed/iq0GMBGdnhc" 
+                     title="Bảo vệ da trong mùa hè | Bản tin Y tế 24h | VTV24| VTV24" 
+                     frameborder="0" 
+                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                     referrerpolicy="strict-origin-when-cross-origin" 
+                     allowfullscreen></iframe>
+                    </div>
+                    <div className="content-right">
+                        <p>Mùa hè với chỉ số UV ngoài chơi thường ở mức cao, bên cạnh đó cũng là mùa nhiều loại côn trùng hoạt động mạnh. Làm sao để chúng ta có thể bảo vệ da trước những tác nhân này?</p>
+                    </div>
+                </div>
             </div>
         );
     }
