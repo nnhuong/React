1556c9477d798e31529b29ea9f79592d64be36af
nnhuong <nnhuong1763@gmail.com>
2024-08-19 20:25:08 +0700

    menu-admin

--- src/containers/Header/menuApp.js
+++ src/containers/Header/menuApp.js
@@ -25,4 +25,28 @@ export const adminMenu = [
 
         ]
     },
+    { //Quản lý phòng khám
+        name: 'menu.admin.clinic', 
+        menus: [
+            {
+            name: 'menu.admin.manage-clinic',  link: '/system/manage-clinic',  
+            },
+        ]
+    },
+    { //Quản lý chuyên khoa
+        name: 'menu.admin.specialty', 
+        menus: [
+            {
+            name: 'menu.admin.manage-specialty',  link: '/system/manage-specialty',  
+            },
+        ]
+    },
+    { //Quản lý cẩm nang
+        name: 'menu.admin.handbook', 
+        menus: [
+            {
+            name: 'menu.admin.manage-handbook',  link: '/system/manage-handbook',  
+            },
+        ]
+    },
 ];
