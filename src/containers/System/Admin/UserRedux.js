da9ede4b7d2dcac3bb67cc45af75485f3772e17e
nnhuong <nnhuong1763@gmail.com>
2024-09-07 21:03:55 +0700

    bootstrap4

--- src/containers/System/Admin/UserRedux.js
+++ src/containers/System/Admin/UserRedux.js
@@ -9,7 +9,7 @@ class UserRedux extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            genderArr: [ ]
+            genderArr: []
         }
     }
 
@@ -17,9 +17,9 @@ class UserRedux extends Component {
     async componentDidMount() {
         this.props.getGenderStart();
 
-        try{
+        try {
             let res = await getAllCodeService('gender');
-            if (res && res.errCode === 0){
+            if (res && res.errCode === 0) {
                 this.setState({
                     genderArr: res.data
                 })
@@ -27,16 +27,16 @@ class UserRedux extends Component {
             console.log('check res: ', res)
         } catch (e) {
             console.log(e)
-        } 
+        }
     }
-    
-    componentDidUpdate (prevProps, prevState, snapshot) {
-        if (prevProps.gender !== this.props.genderRedux){
-            this.setState({
-                genderArr: this.props.genderRedux
-        })
-    }
-}
+
+    //     componentDidUpdate (prevProps, prevState, snapshot) {
+    //         if (prevProps.gender !== this.props.genderRedux){
+    //             this.setState({
+    //                 genderArr: this.props.genderRedux
+    //         })
+    //     }
+    // }
     render() {
         let genders = this.state.genderArr;
         let language = this.props.language;
@@ -44,78 +44,43 @@ class UserRedux extends Component {
         return (
             <div className="user-redux-container">
                 <div className="title">
-                    User Redux
+                    Manager User
                 </div>
                 <div className="user-redux-body">
                     <div className="container">
                         <div className="row">
-                            <div className="col-12 my-3">
-                                <FormattedMessage id="manage-user.add"/>
+                            <div className="col-3">
+                                <label>Email</label>
+                                <input className="form-control" type="email" />
                             </div>
                             <div className="col-3">
-                                <label><FormattedMessage id="manage-user.email"/></label>
-                                <input className="form-control" type="email"/>
+                                <label>Password</label>
+                                <input className="form-control" type="password" />
                             </div>
                             <div className="col-3">
-                                <label><FormattedMessage id="manage-user.password"/></label>
-                                <input className="form-control" type="password"/>
+                                <label>First name</label>
+                                <input className="form-control" type="text" />
                             </div>
                             <div className="col-3">
-                                <label><FormattedMessage id="manage-user.first-name"/></label>
-                                <input className="form-control" type="text"/>
-                            </div> 
+                                <label>Last name</label>
+                                <input className="form-control" type="text" />
+                            </div>
                             <div className="col-3">
-                                <label><FormattedMessage id="manage-user.last-name"/></label>
-                                <input className="form-control" type="text"/>
-                            </div> 
-                            <div className="col-3">
-                                <label><FormattedMessage id="manage-user.phone-number"/></label>
-                                <input className="form-control" type="text"/>
-                            </div> 
+                                <label>Phone number</label>
+                                <input className="form-control" type="text" />
+                            </div>
                             <div className="col-9">
-                                <label><FormattedMessage id="manage-user.address"/></label>
-                                <input className="form-control" type="text"/>
-                            </div> 
-                            <div className="col-3">
-                                <label><FormattedMessage id="manage-user.gender"/></label>
-                            <select className="form-control">
-                            {genders && genders.length > 0 &&
-                                genders.map((item, index) => {
-                                    return (
-                                        <option key={index}>{language === LANGUAGES.VI ? item.valueVi: item.valueEn }</option>
-                                    )
-                                })
-                            }
-                            {/* <option>Nam</option>
-                            <option>Nữ</option>
-                            <option>Khác</option> */}
-
-                            </select>
-                            </div> 
-                            <div className="col-3">
-                                <label><FormattedMessage id="manage-user.position"/></label>
-                            <select className="form-control">
-                                <option selected>Choose...</option>
-                                <option>Admin</option>
-                                <option>Doctor</option>
-                                <option>Patient</option>
-                            </select>
-                            </div> 
-                            <div className="col-3">
-                                <label><FormattedMessage id="manage-user.role"/></label>
-                            <select className="form-control">
-                                <option selected>Choose...</option>
-                                <option>...</option>
-                            </select>
-                            </div> 
-                            <div className="col-3">
-                                <label><FormattedMessage id="manage-user.image"/></label>
-                                <input type="text" className="form-control"/>
-                            </div> 
-                            <div className="col-12">
-                                <button className="btn btn-primary"><FormattedMessage id="manage-user.save"/></button>
+                                <label>Address</label>
+                                <input className="form-control" type="text" />
                             </div>
-                        </div>   
+                            <div className="col-3">
+                                <label>Gender</label>
+                                <select className="form-control">
+                                    <option selected>Choose...</option>
+                                    <option>...</option>
+                                </select>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
