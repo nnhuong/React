0ebec721d86be1d686deb301d659dab0895fd5ea
nnhuong <nnhuong1763@gmail.com>
2024-09-23 22:07:49 +0700

    CRUD with Redux

--- src/store/actions/adminActions.js
+++ src/store/actions/adminActions.js
@@ -1,5 +1,5 @@
 import actionTypes from './actionTypes';
-import { getAllCodeService } from '../../services/userService';
+import { getAllCodeService, createNewUserService } from '../../services/userService';
 
 export const fetchGenderStart = () => {
     return async(dispatch, getState) =>{
@@ -79,4 +79,28 @@ export const fetchRoleStart = () => {
   
 }
 
+export const createNewUser = (data) => {
+    return async(dispatch, getState) =>{
+        try {
+            let res = await createNewUserService(data);
+            console.log('check create user redux: ', res)
+            if (res && res.errCode === 0) {
+                dispatch(saveUserSuccess())
+            } else {
+                dispatch(saveUserFailed());
+            }
+        } catch (e) {
+            dispatch(saveUserFailed());
+            console.log('save user failed: ', e)
+        }
+    }
+}
+
+export const saveUserSuccess = () => ({
+    type: 'CREATE_USER_SUCCESS'
+})
+
+export const saveUserFailed = () => ({
+    type: 'CREATE_USER_FAILED'
+})
 
